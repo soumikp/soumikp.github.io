a478f3655877326f531e76b6743cffb8e858d8a5
Soumik Purkayastha <soumikp@umich.edu>
2022-10-30 08:23:11 -0400

    ami paper updated

---
+++ _publications/2022-10-26-arxiv.R
@@ -0,0 +1,11 @@
+---
+title: "Asymmetric predictability in causal discovery: an information theoretic approach."
+collection: publications
+permalink: /publication/2022-10-26-arxiv
+venue: 'arXiv'
+paperurl: 'https://arxiv.org/pdf/2210.14455'
+citation: 'Purkayastha, Soumik, and Peter XK Song. "Asymmetric predictability in causal discovery: an information theoretic approach." arXiv preprint arXiv:2210.14455 (2022).'
+---
+  
+**Abstract:** Causal investigations in observational studies pose a great challenge in scientific research where randomized trials or intervention-based studies are not feasible. Leveraging Shannon's seminal work on information theory, we develop a causal discovery framework of "predictive asymmetry" for bivariate (X,Y). Predictive asymmetry is a central concept in information geometric causal inference; it enables assessment of whether X is a stronger predictor of Y or vice-versa. We propose a new metric called the Asymmetric Mutual Information (AMI) and establish its key statistical properties. The AMI is not only able to detect complex non-linear association patterns in bivariate data, but also is able to detect and quantify predictive asymmetry. Our proposed methodology relies on scalable non-parametric density estimation using fast Fourier transformation. The resulting estimation method is manyfold faster than the classical bandwidth-based density estimation, while maintaining comparable mean integrated squared error rates. We investigate key asymptotic properties of the AMI methodology; a new data-splitting technique is developed to make statistical inference on predictive asymmetry using the AMI. We illustrate the performance of the AMI methodology through simulation studies as well as multiple real data examples.
+
